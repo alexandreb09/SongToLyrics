36fff5aaebc73a9c3af9687f0b8ac6500e0bd193
Alexandre <alexandreb09@yahoo.fr>
2020-01-13 16:08:30 +0100

    :tada: First Commit :tada:

---
+++ app/src/main/java/com/example/songtolyrics/EnregistrementActivity.java
@@ -0,0 +1,13 @@
+package com.example.songtolyrics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnregistrementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement);
+    }
+}
---
+++ app/src/main/java/com/example/songtolyrics/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.songtolyrics;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_nouvelle_recherche;
+    Button btn_historique;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_nouvelle_recherche = findViewById(R.id.homepage_btn_record);
+        btn_historique = findViewById(R.id.homepage_btn_historique);
+
+        btn_nouvelle_recherche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nouvelle_recherche = new Intent(getApplicationContext(), EnregistrementActivity.class); // Création nouvelle intent
+                startActivity(nouvelle_recherche);                                                      // Lancement nouvelle activité
+            }
+        });
+
+        btn_historique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent historique = new Intent(getApplicationContext(), HistoriqueActivity.class); // Création nouvelle intent
+                startActivity(historique);                                                      // Lancement nouvelle activité
+            }
+        });
+
+    }
+}
